8cc34756ee7fc36accc32ee409a38110dd42d4cc
Niles <niulinguo@163.com>
2018-10-19 16:47:49 +0800

    登录认证

--- app/src/main/java/com/niles/huaweiloginmodule/MainActivity.java
+++ app/src/main/java/com/niles/huaweiloginmodule/MainActivity.java
@@ -3,6 +3,7 @@ package com.niles.huaweiloginmodule;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.niles.huawei_login.AuthResultListener;
@@ -11,18 +12,27 @@ import com.niles.huawei_login.LoginManager;
 
 public class MainActivity extends AppCompatActivity implements AuthResultListener {
 
+    private EditText mUsernameEditView;
+    private EditText mPasswordEditView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUsernameEditView = findViewById(R.id.et_username);
+        mPasswordEditView = findViewById(R.id.et_password);
+
+        mUsernameEditView.setText(BuildConfig.USERNAME);
+        mPasswordEditView.setText(BuildConfig.PASSWORD);
     }
 
     public void onButtonClicked(View view) {
         LoginManager.doAuthentication(this, new LoginConfig.Builder()
                 .setAuthInBackground(true)
                 .setGateway(BuildConfig.GATEWAY)
-                .setUsername(BuildConfig.USERNAME)
-                .setPassword(BuildConfig.PASSWORD)
+                .setUsername(mUsernameEditView.getText().toString())
+                .setPassword(mPasswordEditView.getText().toString())
                 .setAuthResultListener(this)
                 .build());
     }
--- huawei_login/src/main/java/com/niles/huawei_login/LoginManager.java
+++ huawei_login/src/main/java/com/niles/huawei_login/LoginManager.java
@@ -1,20 +1,14 @@
 package com.niles.huawei_login;
 
-import android.app.Application;
 import android.content.Context;
-import android.util.Log;
 
-import com.huawei.anyoffice.sdk.SDKContext;
-import com.huawei.anyoffice.sdk.SDKContextOption;
-import com.huawei.anyoffice.sdk.network.NetChangeCallback;
-import com.huawei.anyoffice.sdk.network.NetStatusManager;
+import com.huawei.anyoffice.sdk.log.Log;
 import com.huawei.esdk.byod.anyoffice.AnyOfficeSDK;
+import com.huawei.esdk.byod.anyoffice.AnyOfficeUserInfo;
 import com.niles.huawei_login.scene.SDKAuthenticator;
 import com.niles.huawei_login.scene.SDKAuthenticatorFactory;
 import com.niles.huawei_login.scene.SDKConfiguration;
 import com.niles.huawei_login.scene.SDKInitializer;
-
-import java.io.File;
 
 /**
  * Created by Niles
@@ -24,12 +18,6 @@ import java.io.File;
 public class LoginManager {
 
     private static final String TAG = "LoginManager";
-    private static final NetChangeCallback sNetChangeCallback = new NetChangeCallback() {
-        @Override
-        public void onNetChanged(int oldState, int newState, int errorCode) {
-            Log.e(TAG, String.format("oldState:%d, newState:%d, errorCode:%d, ", oldState, newState, errorCode));
-        }
-    };
     private static int authStatus = 0;
     private static int appVpnStatus = IAppVpnStatusObserver.VPN_STATUS_CONNECTED;
     private static int sdkAuthResult = 0;
@@ -39,10 +27,10 @@ public class LoginManager {
         if (sdkAuthResult == 0 && appVpnStatus == IAppVpnStatusObserver.VPN_STATUS_CONNECTED) {
             tmpResult = 0;
         } else if (sdkAuthResult != 0) {
-            com.huawei.anyoffice.sdk.log.Log.e(TAG, "sdk authentication error: " + sdkAuthResult);
+            Log.e(TAG, "sdk authentication error: " + sdkAuthResult);
             tmpResult = sdkAuthResult;
         } else {
-            com.huawei.anyoffice.sdk.log.Log.e(TAG, "app vpn error: " + appVpnStatus);
+            Log.e(TAG, "app vpn error: " + appVpnStatus);
             tmpResult = appVpnStatus - 1;
         }
 
@@ -50,32 +38,23 @@ public class LoginManager {
         if (result == 0) {
             if (SDKConfiguration.getInstance().sdkMode == SDKConfiguration.SDK_MODE_L4VPN) {
                 String tunnelIp = AnyOfficeSDK.getTunnelIPAddress();
+                AnyOfficeUserInfo userInfo = AnyOfficeSDK.getUserInfo();
+                Log.e(TAG, userInfo.toString());
             }
         }
         loginConfig.getAuthResultListener().onResult(result);
     }
 
-    public static void init(Application app) {
-        //SDK初始化设置
-        SDKContext ctx = SDKContext.getInstance();
-        //设置网络回调，用于接收隧道状态变化通知
-        NetStatusManager.getInstance().setNetChangeCallback(sNetChangeCallback);
-        //设置SDK日志和输出目录
-        String sdkLogPath = new File(app.getFilesDir(), "log").getAbsolutePath();
-        SDKContext.getInstance().setLogParam(sdkLogPath, com.huawei.anyoffice.sdk.log.Log.LOG_TYPE_DEBUG);
-        SDKContextOption option = new SDKContextOption();
-        //设置SDK工作目录
-        String workpath = new File(app.getFilesDir(), "anyoffice").getAbsolutePath();
-        option.setWorkPath(workpath);
-        option.setContext(app);
-        //执行SDK初始化
-        boolean ret = ctx.init(option);
-        Log.e(TAG, "AnyOffice Init Result:" + ret);
-    }
-
     public static void doAuthentication(Context context, LoginConfig loginConfig) {
+
         SDKInitializer initializer = SDKInitializer.getInstance();
         initializer.init(context);
+
+        AnyOfficeUserInfo userInfo = AnyOfficeSDK.getUserInfo();
+        if (userInfo != null && loginConfig.getUsername().equals(userInfo.getUsername())) {
+            loginConfig.getAuthResultListener().onResult(0);
+            return;
+        }
 
         authStatus = 0x2;
         if (SDKConfiguration.getInstance().sdkMode == SDKConfiguration.SDK_MODE_APPVPN) {
